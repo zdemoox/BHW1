4612d70d591441c8df243d6c190f48ba5377c21d
zdemoox <mikael5555556@gmail.com>
2025-03-21 23:29:28 +0300

    Add files via upload

---
+++ Visitors/IExportVisitor.cs
@@ -0,0 +1,12 @@
+﻿using FinancialAccounting.Models;
+
+namespace FinancialAccounting.Visitors
+{
+    public interface IExportVisitor
+    {
+        string Visit(BankAccount account);
+        string Visit(Category category);
+        string Visit(Operation operation);
+        string Export(IEnumerable<object> items);
+    }
+}
---
+++ Visitors/JsonExportVisitor.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using FinancialAccounting.Models;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace FinancialAccounting.Visitors
+{
+    public class JsonExportVisitor : IExportVisitor
+    {
+        private readonly JsonSerializerOptions _options = new()
+        {
+            WriteIndented = true,
+            Converters = { new JsonStringEnumConverter() }
+        };
+
+        public string Export(IEnumerable<object> items)
+        {
+            var wrappedItems = items.Select(item =>
+            {
+                string type = item switch
+                {
+                    BankAccount => "BankAccount",
+                    Category => "Category",
+                    Operation => "Operation",
+                    _ => throw new NotSupportedException()
+                };
+
+                return new
+                {
+                    type, 
+                    Data = item
+                };
+            });
+
+            return JsonSerializer.Serialize(wrappedItems, _options);
+        }
+
+        public string Visit(BankAccount account) =>
+            JsonSerializer.Serialize(account, _options);
+
+        public string Visit(Category category) =>
+            JsonSerializer.Serialize(category, _options);
+
+        public string Visit(Operation operation) =>
+            JsonSerializer.Serialize(operation, _options);
+    }
+}
