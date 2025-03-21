8434544ffcd023b7c49718c2cb2b6e7833a7b499
zdemoox <mikael5555556@gmail.com>
2025-03-21 23:25:25 +0300

    Add files via upload

---
+++ Modes/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace FinancialAccounting.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public OperationType Type { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Modes/Operation.cs
@@ -0,0 +1,13 @@
+﻿namespace FinancialAccounting.Models
+{
+    public class Operation
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public OperationType Type { get; set; }
+        public Guid BankAccountId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
