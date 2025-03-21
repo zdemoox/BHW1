5a1d6d643037114708cf556db9b0eaf59ee1122d
zdemoox <mikael5555556@gmail.com>
2025-03-21 23:07:16 +0300

    Add files via upload

---
+++ AddOperationCommand.cs
@@ -0,0 +1,18 @@
+﻿using FinancialAccounting.Models;
+using FinancialAccounting.Services;
+
+public class AddOperationCommand : ICommand
+{
+    private readonly IOperationService _service;
+    private readonly Operation _operation;
+    private readonly BankAccountService _accountService;
+
+    public AddOperationCommand(IOperationService service, Operation operation, BankAccountService accountService)
+    {
+        _service = service;
+        _operation = operation;
+        _accountService = accountService;
+    }
+
+    public void Execute() => _service.AddOperation(_operation, _accountService);
+}
